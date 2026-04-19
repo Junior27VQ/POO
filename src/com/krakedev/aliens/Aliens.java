c8a8d505b0c4a09c6362cdd44b82a6a18c91671d
Junior27VQ <favq1999@outlook.com>
2026-04-18 19:42:48 -0500

    New alien project and encapsulation of attributes

---
+++ src/com/krakedev/aliens/Aliens.java
@@ -0,0 +1,15 @@
+package com.krakedev.aliens;
+
+public class Aliens {
+	private int tamanio;
+	private String color;
+	private int numeroOjos;
+	private int numeroBrazos;
+	private int numeroPies;
+	private double precioExtremidad;
+	private double precioOjos;
+	private double precioCuerpo;
+	
+	
+
+}
