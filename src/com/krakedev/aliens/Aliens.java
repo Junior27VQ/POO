6ab47e214acc1af2d4e4317051847baf7bf42377
Junior27VQ <favq1999@outlook.com>
2026-04-18 19:45:15 -0500

    Part 3: Creating Constructors and Validations

--- src/com/krakedev/aliens/Aliens.java
+++ src/com/krakedev/aliens/Aliens.java
@@ -10,6 +10,18 @@ public class Aliens {
 	private double precioOjos;
 	private double precioCuerpo;
 	
-	
+	public Aliens(int tamanio, String color) {
+		super();
+		if(tamanio<=5 & tamanio>=30) {
+			this.tamanio = tamanio;
+			this.color = color;
+		}else {
+			this.tamanio = 25;
+			this.color = color;
+		}
+		this.precioCuerpo=this.tamanio*0.20;
+		this.precioExtremidad=this.tamanio*0.10;
+		this.precioOjos=this.tamanio*0.05;
+	}
 
 }
